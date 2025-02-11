a9abb4d2405f44210e9f6c43dcfb58deaabf34ba
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-02-11 15:48:55 +0100

    Tussentijdse commit - left off at One Way platform.

---
+++ ProefGame/Assets/PlayerOneWayPlatform.cs
@@ -0,0 +1,45 @@
+using System.Collections; // Added for IEnumerator
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneWayPlatform : MonoBehaviour
+{
+    private GameObject currentOneWayPlatform;
+    [SerializeField] private BoxCollider2D playerCollider;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        { 
+            if (currentOneWayPlatform != null)
+            {
+                StartCoroutine(DisableCollision());
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("OneWayPlatform")) // Fixed: changed 'collision' to 'other'
+        {
+            currentOneWayPlatform = other.gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = null;
+        }
+    }
+
+    private IEnumerator DisableCollision()
+    {
+        BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+        
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(0.25f);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+    }
+}
